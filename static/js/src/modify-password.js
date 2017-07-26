1570c31585661b1687b45923ca9e5e10490811fd
lynn_tee <lynn_tee@outlook.com>
2017-07-26 21:15:20 +0800

    增加发布需求帖API, 未完成

---
+++ static/js/src/modify-password.js
@@ -0,0 +1,24 @@
+$(function () {
+  $('#change-password').click(function (event) {
+    event.preventDefault()
+    var $oldPassword = $('#old-password').val()
+    var $newPassword = $('#new-password').val()
+    $.ajax({
+      url: '/api/v1/modify-password',
+      data: JSON.stringify({old_password: $oldPassword, new_password: $newPassword}),
+      contentType: 'application/json',
+      type: 'POST',
+      dataType: 'json',
+      headers: {'Authorization': 'jwt' + ' ' + sessionStorage.getItem('jwt')},
+      success: function (msg) {
+        console.log('modify password success!')
+        // console.log(sessionStorage.getItem('jwt')) // for debug
+      },
+      error: function (error_msg) {
+        console.log(error_msg)
+        console.log('modify password failed!')
+        // console.log(sessionStorage.getItem('jwt')) // for debug
+      }
+    })
+  })
+})
